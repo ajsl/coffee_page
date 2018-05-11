68ab7ab8d30ab4d46cdcd14a1901c65d2ba872e7
James Leith <james@Jamess-MacBook-Pro.local>
2018-05-11 10:39:33 +0100

    started js changed

--- js/all.js
+++ js/all.js
@@ -14465,27 +14465,3 @@ return exports;
 
 }({},$,Popper));
 //# sourceMappingURL=bootstrap.js.map
-
-/*
- * Some JavaScript for counting the number of characters left
- * in the Contact Form's Message box
- * https://bootsnipp.com/snippets/featured/simple-contact-form
- */
-$(document).ready(function(){ 
-    $('#characterLeft').text('140 characters left');
-    $('#message').keydown(function () {
-        var max = 140;
-        var len = $(this).val().length;
-        if (len >= max) {
-            $('#characterLeft').text('You have reached the limit');
-            $('#characterLeft').addClass('red');
-            $('#btnSubmit').addClass('disabled');            
-        } 
-        else {
-            var ch = max - len;
-            $('#characterLeft').text(ch + ' characters left');
-            $('#btnSubmit').removeClass('disabled');
-            $('#characterLeft').removeClass('red');            
-        }
-    });    
-});
