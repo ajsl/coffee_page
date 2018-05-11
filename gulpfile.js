4745160104103f5671b4430293faab3780df2cd3
Simon Lewin <simon@MacBook-Pro-GJ93G.local>
2018-05-11 12:17:04 +0100

    gulp watch tests for indent = 4 spaces  using ESLint instead of JSCS

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+// gulpfile.js Coffee World project
+//
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
@@ -7,7 +9,8 @@ let gulpSequence = require('gulp-sequence');
 let concat = require('gulp-concat');
 let uglify = require('gulp-uglify-es').default;
 
-
+// Compile sass files into ./css/styles.css
+//
 gulp.task('sass', function () {
     var stream = gulp.src('./scss/styles.scss')
         .pipe(sass())
@@ -16,6 +19,8 @@ gulp.task('sass', function () {
     return stream;
 });
 
+// Minify css in styles.css and name ./css/styles.min.css
+//
 gulp.task('minify-css', () => {
   return gulp.src('css/styles.css')
 	.pipe(cleanCSS({compatibility: 'ie8'}))
@@ -23,16 +28,30 @@ gulp.task('minify-css', () => {
 	.pipe(gulp.dest('./css/'));
 });
 
+// Combine compile sass and minify tasks into one gulp styles task
+//
 gulp.task('styles', function(callback){
 	gulpSequence('sass', 'minify-css')(callback)
 });
 
+// Assign js files to be concatenated, order is important:
+// jQuery first, then Popper.js, then Bootstrap JS, then any other js
+//
+let js_files = ["./js/jquery-3.2.1.slim.js",
+                "./js/popper.js", 
+                "./js/bootstrap.js", 
+                "./js/contact.js"]
+
+// Concatenate js files into ./js/all.js
+//
 gulp.task('concat-js', function() {
-  return gulp.src(["./js/jquery-3.2.1.slim.js", "./js/popper.js", "./js/bootstrap.js", "./js/contact.js"])
+  return gulp.src(js_files)
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./js/'));
 });
 
+// Uglify (minify) .js/all.js file and name ./js/all.min.js
+//
 gulp.task('uglify', function () {
     return gulp.src("./js/all.js")
         .pipe(rename({suffix: '.min'}))
@@ -40,53 +59,34 @@ gulp.task('uglify', function () {
         .pipe(gulp.dest("./js/"));
 });
 
+// Combine concat and uglify js files tasks into one js-style task
+//
 gulp.task('js-style', function(callback){
     gulpSequence('concat-js', 'uglify')(callback)
 })
 
-
+// gulp watch task to run gulp js-styles and gulp styles if
+// anything changes 
+//
 gulp.task('watch', function () {
-	gulp.watch(['./js/jquery-3.2.1.slim.js', './js/popper.js', './js/bootstrap.js', "./js/contact.js"], ['js-style'])
-	// gulp.watch('./js/all.js', ['uglify'])
+	gulp.watch(js_files, ['js-style']);
+    gulp.watch(js_files_test, ['lint']);
 	gulp.watch('./scss/*.scss', ['styles']);
 });
 
-
-
-const jscs = require('gulp-jscs');
- 
-gulp.task('report', () => {
-    return gulp.src('js/all.js')
-        .pipe(jscs())
-        .pipe(jscs.reporter());
-});
-
-gulp.task('fix', () => {
-    return gulp.src('js/all.js')
-        .pipe(jscs({fix: true}))
-        .pipe(gulp.dest('js/test'));
-});
-
+// Check JavaScript code style with ESLint
+//
 const eslint = require('gulp-eslint');
- 
+
+let js_files_test = ["./js/contact.js"];
+
 gulp.task('lint', () => {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-    return gulp.src(['/js/*.js','!node_modules/**'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
+    return gulp.src(js_files_test)
         .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
         .pipe(eslint.failAfterError());
 });
  
-gulp.task('default', ['lint'], function () {
-    // This will only run if the lint task is successful...
-});
-
+// gulp.task('default', ['lint'], function () {
+//     // This will only run if the lint task is successful...
+// });
